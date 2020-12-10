77bbe25e8f6de8a103d46e378d8af86136f2cfe1
shirleyndou <shirley.ndou@umuzi.org>
2020-12-10 03:31:33 +0200

    GOL to be modified

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,16 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         ConwayGameOfLife gol = new ConwayGameOfLife(3, 6);
-
         gol.printCells(); //prints out 1st generation
-
         gol.nextGeneration();
-
         gol.printCells(); //prints out second generation
-
         gol.nextGeneration();
-
-        //gol.printCells();
 
 
     }
